e7ea0b91d77d89ea6a5ef66abb90d15c08800110
Stefano Sangalli <stefano.sangalli@midainformatica.it>
2018-07-03 14:12:30 +0200

    Aggiunto cambio titolo al Load

--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,9 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "Welcome to Fibonacci application"
+    End Sub
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim _result As New List(Of Integer)
         _result = GetFibonacci(CType(txtN.Text, Integer))
